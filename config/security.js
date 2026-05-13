c6fa9c5db9ed2f04152dff8bb07c261ff9d4ec43
Hoàng Quang Thắng <thanghoang182004@gmail.com>
2026-05-13 23:00:56 +0700

    fix cors1

--- config/security.js
+++ config/security.js
@@ -4,13 +4,10 @@ module.exports.security = {
     allowOrigins: [
       'https://taskflow-frontend-virid.vercel.app',
       'https://taskflow-frontend.vercel.app',
-      'http://localhost:3000',
-      'https://taskflow-frontend-*.vercel.app', 
-      '*'  
+      'http://localhost:3000'
     ],
     allowCredentials: true,
-    allowRequestMethods: 'GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD',
-    allowRequestHeaders: 'content-type, authorization, x-csrf-token, accept, origin',
-    allowResponseHeaders: 'content-type, authorization',
+    allowRequestMethods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+    allowRequestHeaders: 'content-type, authorization',
   },
 };
